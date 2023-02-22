3a3c4a031e4bb9ed9697039e3f1af72f625f68dd
ruirajao <122367154+ruirajao@users.noreply.github.com>
2023-02-22 15:48:09 +0000

    HttpHeaderBuilder contentType

--- src/utils/HttpHeaderBuilder.java
+++ src/utils/HttpHeaderBuilder.java
@@ -14,7 +14,22 @@ public class HttpHeaderBuilder {
                 "Content-Length: " + length + "\r\n\r\n";
     }
 
-    public static String contentType(String fileName){
-        return "a";
+    public static String contentType(String fileName) {
+        String extension = fileName.substring(fileName.lastIndexOf("."));
+        switch (extension) {
+            case ".html":
+                return "Content-Type: text/html\r\n";
+            case ".mp3":
+                return "Content-Type: audio/mpeg\r\n";
+            case ".gif":
+                return "Content-Type: image/gif\r\n";
+            case ".jpeg":
+            case "jpg":
+                return "Content-Type: image/jpeg\r\n";
+            case  ".txt":
+                return "Content-Type: text/plain\r\n";
+            default:
+                return "Content-Type: application/octet-stream\r\n";
+        }
     }
 }
