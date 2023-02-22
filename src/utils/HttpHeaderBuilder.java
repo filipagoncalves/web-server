eae21a6274b703541b3179de19e84919c6b5dab0
ruirajao <122367154+ruirajao@users.noreply.github.com>
2023-02-22 16:05:14 +0000

    HttpHeaderBuilder methods

--- src/utils/HttpHeaderBuilder.java
+++ src/utils/HttpHeaderBuilder.java
@@ -32,4 +32,20 @@ public class HttpHeaderBuilder {
                 return "Content-Type: application/octet-stream\r\n";
         }
     }
+
+    public static String badRequest (String fileName, long length){
+        return "HTTP/1.0 400 Bad Request\r\n" +
+                contentType(fileName) + length + "Bad Request: " + length + "\r\n\r\n";
+    }
+
+    public static String internalError (String fileName, long length){
+        return "HTTP/1.0 500 Internal Server Error\r\n" + "Internal Server Error: " + length + "\r\n\r\n";
+    }
+
+    public static String temporaryRedirect (String fileName, long length){
+        return "HTTP/1.0 307 Temporary Redirect \r\n" + "Temporary Redirect: " + length + "\r\n\r\n";
+    }
+    public static String movedPermanently (String fileName, long length){
+        return "HTTP/1.0 301 Moved Permanently\r\n" + "Moved Permanently: " + length + "\r\n\r\n";
+    }
 }
